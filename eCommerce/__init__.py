5d709092dadb3b8f63050d72bc43cc0164098c39
Jahankir Kabeer <109400759+Jahankir@users.noreply.github.com>
2023-11-28 11:20:07 +0530

    Update __init__.py

--- eCommerce/__init__.py
+++ eCommerce/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '75b73ad469e9c65a472e2f46bdbc7260c87e3850'
+app.config['SECRET_KEY'] = 'some secret key'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/ecommerce'
 db = PyMongo(app).db
 bcrypt = Bcrypt(app)
